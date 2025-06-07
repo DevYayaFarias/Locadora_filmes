78ca7c1a8ca919d408f447fc4862bc5c5b8a8455
DevYayaFarias <minvicflg@gmail.com>
2025-06-06 23:56:12 -0300

    Update Form.py
    Código não finalizado.

--- Back-End/Form.py
+++ Back-End/Form.py
@@ -1,1 +1,11 @@
 
+def recomendar_filmes(catalogo):
+    print("*-*-*- Bem-vindo à nossa locadora de filmes!! -*-*-*")
+
+    genero = input("Digite o gênero de sua preferência: ").strip().lower()
+    ano = input("Digite o ano de sua preferência (ou deixe em branco): ").strip()
+
+    recomendados = [}
+
+catalogo = []
+recomendar_filmes(catalogo)
