b523f065e405d69a777ab5f238d95aab5bfafda1
DevYayaFarias <minvicflg@gmail.com>
2025-06-10 09:23:48 -0300

    Update Form.py

--- Back-End/Form.py
+++ Back-End/Form.py
@@ -5,12 +5,22 @@ def recomendar_filmes(catalogo):
     genero = input("Digite o gênero de sua preferência: ").strip().lower()
     ano = input("Digite o ano de sua preferência (ou deixe em branco): ").strip()
     
-     recomendados = []
+    recomendados = []
 
 
     for filme in catalogo:
+        genero_correto = genero.lower() in filme['genero'].lower() if genero else True
+        ano_correto = filme['ano'] == ano  if ano else True
 
-        
+        if genero_correto and ano_correto:
+            recomendados.append(filme)
+
+    if recomendados:
+        print("\n Filmes recomendados para você: ")
+        for filme in recomendados:
+            print(f"-{filme['titulo']} ({filme['ano']}) - Gênero: {filme['genero']}")
+    else:
+        print("\nDesculpe, não encontramos filmes com essas preferências")
 
 catalogo = []
 recomendar_filmes(catalogo)
