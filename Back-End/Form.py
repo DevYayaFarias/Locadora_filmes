de2a66a3ce5ab2bfcac71820c58000f667d8e23a
DevYayaFarias <minvicflg@gmail.com>
2025-06-12 09:25:19 -0300

    Update Form.py
    Adição do catalogo de filmes

--- Back-End/Form.py
+++ Back-End/Form.py
@@ -1,26 +1,68 @@
-
+catalogo = {
+    "Matrix": {"ano": 1999, "genero": "Ficção", "disponibilidade": "disponível", "diretor": "Lana Wachowski, Lilly Wachowski", "empresa_produtora": "Warner Bros.", "avaliação": 87},
+    "Titanic": {"ano": 1997, "genero": "Romance", "disponibilidade": "disponível", "diretor": "James Cameron", "empresa_produtora": "Paramount Pictures", "avaliação": 89},
+    "Vingadores": {"ano": 2012, "genero": "Ação", "disponibilidade": "disponível", "diretor": "Joss Whedon", "empresa_produtora": "Marvel Studios", "avaliação": 91},
+    "Shrek": {"ano": 2001, "genero": "Animação", "disponibilidade": "disponível", "diretor": "Andrew Adamson, Vicky Jenson", "empresa_produtora": "DreamWorks Animation", "avaliação": 88},
+    "Coringa": {"ano": 2019, "genero": "Drama", "disponibilidade": "disponível", "diretor": "Todd Phillips", "empresa_produtora": "Warner Bros.", "avaliação": 68},
+    "Interestelar": {"ano": 2014, "genero": "Ficção", "disponibilidade": "disponível", "diretor": "Christopher Nolan", "empresa_produtora": "Paramount Pictures", "avaliação": 86},
+    "O Senhor dos Anéis": {"ano": 2001, "genero": "Fantasia", "disponibilidade": "disponível", "diretor": "Peter Jackson", "empresa_produtora": "New Line Cinema", "avaliação": 91},
+    "Harry Potter": {"ano": 2001, "genero": "Fantasia", "disponibilidade": "disponível", "diretor": "Chris Columbus", "empresa_produtora": "Warner Bros.", "avaliação": 81},
+    "Star Wars": {"ano": 1977, "genero": "Ficção", "disponibilidade": "disponível", "diretor": "George Lucas", "empresa_produtora": "Lucasfilm", "avaliação": 92},
+    "Jurassic Park": {"ano": 1993, "genero": "Aventura", "disponibilidade": "disponível", "diretor": "Steven Spielberg", "empresa_produtora": "Universal Pictures", "avaliação": 91},
+    "O Poderoso Chefão": {"ano": 1972, "genero": "Crime", "disponibilidade": "disponível", "diretor": "Francis Ford Coppola", "empresa_produtora": "Paramount Pictures", "avaliação": 98},
+    "Casablanca": {"ano": 1942, "genero": "Romance", "disponibilidade": "disponível", "diretor": "Michael Curtiz", "empresa_produtora": "Warner Bros.", "avaliação": 97},
+    "O Senhor dos Anéis: As Duas Torres": {"ano": 2002, "genero": "Fantasia", "disponibilidade": "disponível", "diretor": "Peter Jackson", "empresa_produtora": "New Line Cinema", "avaliação": 95},
+    "O Senhor dos Anéis: O Retorno do Rei": {"ano": 2003, "genero": "Fantasia", "disponibilidade": "disponível","diretor": "Peter Jackson", "empresa_produtora": "New Line Cinema", "avaliação": 93},
+    "O Grande Lebowski": {"ano": 1998, "genero": "Comédia", "disponibilidade": "disponível", "diretor": "Joel Coen, Ethan Coen", "empresa_produtora": "PolyGram Filmed Entertainment", "avaliação": 83},
+    "Pulp Fiction": {"ano": 1994, "genero": "Crime", "disponibilidade": "disponível", "diretor": "Quentin Tarantino", "empresa_produtora": "Miramax Films", "avaliação": 92},
+    "O Rei Leão": {"ano": 1994, "genero": "Animação", "disponibilidade": "disponível", "diretor": "Roger Allers, Rob Minkoff", "empresa_produtora": "Walt Disney Feature Animation", "avaliação": 93},
+    "Forrest Gump": {"ano": 1994, "genero": "Drama", "disponibilidade": "disponível", "diretor": "Robert Zemeckis", "empresa_produtora": "Paramount Pictures", "avaliação": 71},
+    "O Silêncio dos Inocentes": {"ano": 1991, "genero": "Terror", "disponibilidade": "disponível", "diretor": "Jonathan Demme", "empresa_produtora": "Orion Pictures", "avaliação": 96},
+    "Clube da Luta": {"ano": 1999, "genero": "Drama", "disponibilidade": "disponível", "diretor": "David Fincher", "empresa_produtora": "20th Century Fox", "avaliação": 79},
+    "O Exorcista": {"ano": 1973, "genero": "Terror", "disponibilidade": "disponível", "diretor": "William Friedkin", "empresa_produtora": "Warner Bros.", "avaliação": 84},
+    "O Iluminado": {"ano": 1980, "genero": "Terror", "disponibilidade": "disponível", "diretor": "Stanley Kubrick", "empresa_produtora": "Warner Bros.", "avaliação": 93},
+    "O Labirinto do Fauno": {"ano": 2006, "genero": "Fantasia", "disponibilidade": "disponível", "diretor": "Guillermo del Toro", "empresa_produtora": "Warner Bros.", "avaliação": 95},
+    "O Pianista": {"ano": 2002, "genero": "Drama", "disponibilidade": "disponível", "diretor": "Roman Polanski", "empresa_produtora": "Canal+", "avaliação": 85},
+    "O Cavaleiro das Trevas": {"ano": 2008, "genero": "Ação", "disponibilidade": "disponível", "diretor": "Christopher Nolan", "empresa_produtora": "Warner Bros.", "avaliação": 94},
+    "A Origem": {"ano": 2010, "genero": "Ficção", "disponibilidade": "disponível", "diretor": "Christopher Nolan", "empresa_produtora": "Warner Bros.", "avaliação": 86},
+    "O Lobo de Wall Street": {"ano": 2013, "genero": "Comédia", "disponibilidade": "disponível", "diretor": "Martin Scorsese", "empresa_produtora": "Paramount Pictures", "avaliação": 79},
+    "A Lista de Schindler": {"ano": 1993, "genero": "Drama", "disponibilidade": "disponível", "diretor": "Steven Spielberg", "empresa_produtora": "Universal Pictures", "avaliação": 93},
+    "O Poderoso Chefão: Parte II": {"ano": 1974, "genero": "Crime", "disponibilidade": "disponível", "diretor": "Francis Ford Coppola", "empresa_produtora": "Paramount Pictures", "avaliação": 97},
+    "O Poderoso Chefão: Parte III": {"ano": 1990, "genero": "Crime", "disponibilidade": "disponível", "diretor": "Francis Ford Coppola", "empresa_produtora": "Paramount Pictures", "avaliação": 68},
+    "O Grande Hotel Budapeste": {"ano": 2014, "genero": "Comédia", "disponibilidade": "disponível", "diretor": "Wes Anderson", "empresa_produtora": "Fox Searchlight Pictures", "avaliação": 91},
+    "O Menino do Pijama Listrado": {"ano": 2008, "genero": "Drama", "disponibilidade": "disponível", "diretor": "Mark Herman", "empresa_produtora": "Miramax Films", "avaliação": 74},
+    "O Regresso": {"ano": 2015, "genero": "Aventura", "disponibilidade": "disponível", "diretor": "Alejandro González Iñárritu", "empresa_produtora": "20th Century Fox", "avaliação": 78},
+    "O Discurso do Rei": {"ano": 2010, "genero": "Drama", "disponibilidade": "disponível", "diretor": "Tom Hooper", "empresa_produtora": "The Weinstein Company", "avaliação": 86},
+    "O Labirinto de Pan": {"ano": 2006, "genero": "Fantasia", "disponibilidade": "disponível", "diretor": "Guillermo del Toro", "empresa_produtora": "Warner Bros.", "avaliação": 95},
+    "O Grande Truque": {"ano": 2006, "genero": "Mistério", "disponibilidade": "disponível", "diretor": "Christopher Nolan", "empresa_produtora": "Warner Bros.", "avaliação": 92},
+    "O Quarto de Jack": {"ano": 2015, "genero": "Drama", "disponibilidade": "disponível", "diretor": "Lenny Abrahamson", "empresa_produtora": "Element Pictures", "avaliação": 93}
+}
 def recomendar_filmes(catalogo):
     print("*-*-*- Bem-vindo à nossa locadora de filmes!! -*-*-*")
-
     genero = input("Digite o gênero de sua preferência: ").strip().lower()
-    ano = input("Digite o ano de sua preferência (ou deixe em branco): ").strip()
-    
+    diretor = input("Digite o diretor de sua preferência (opcional): ").strip().lower()
+    empresa_produtora = input("Digite a empresa produtora de sua preferência (opcional): ").strip().lower()
+    ano = input("Digite o ano de sua preferência (opcional): ").strip()
+    avaliação = input("Digite a avaliação de sua preferência (opcional): ").strip()
+
     recomendados = []
 
+    for titulo, dados in catalogo.items():
+        genero_correto = genero in dados['genero'].lower() if genero else True
+        diretor_correto = diretor in dados['diretor'].lower() if diretor else True
+        empresa_produtora_correta = empresa_produtora in dados['empresa_produtora'].lower() if empresa_produtora else True
+        ano_correto = str(dados['ano']) == ano if ano else True
+        avaliação_correta = str(dados['avaliação']) == avaliação if avaliação else True
 
-    for filme in catalogo:
-        genero_correto = genero.lower() in filme['genero'].lower() if genero else True
-        ano_correto = filme['ano'] == ano  if ano else True
 
-        if genero_correto and ano_correto:
-            recomendados.append(filme)
+        if genero_correto and ano_correto and diretor_correto and empresa_produtora_correta and avaliação_correta:
+            recomendados.append((titulo, dados))
 
     if recomendados:
-        print("\n Filmes recomendados para você: ")
-        for filme in recomendados:
-            print(f"-{filme['titulo']} ({filme['ano']}) - Gênero: {filme['genero']}")
+        print("\nFilmes recomendados para você:")
+        for titulo, dados in recomendados:
+            print(f"- {titulo} ({dados['ano']}) - Gênero: {dados['genero']}")
     else:
-        print("\nDesculpe, não encontramos filmes com essas preferências")
+        print("\nDesculpe, não encontramos filmes com essas preferências.")
 
-catalogo = []
 recomendar_filmes(catalogo)
