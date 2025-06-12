d973bb6ee25392ab9a2ba225ab7b53e8d5e3054b
giovannavazporfirio <giovannavazporfirio@gmail.com>
2025-06-12 09:02:42 -0300

    Add files via upload
    O que foi alterado: Foi adicionado a opção de login; bibliotecas para salvar o cadastro e chamar no login

--- Back-End/cadastro.py
+++ Back-End/cadastro.py
@@ -1,8 +1,11 @@
+import os
+import json
+
 def cadastro_cliente():
     nome = input("Qual o seu nome? ")
     cpf = input("Qual seu CPF? ")
     if len(cpf) == 11:
-        pass
+        pass 
     data_nascimento = input("Digite sua data de nascimento: ")
     email = input("Digite seu email: ")
     senha = input("Crie uma senha: ")
@@ -16,7 +19,6 @@ def cadastro_cliente():
     }
 
     return cliente
-
 
 def cadastro_adm():
     nome = input("Qual o seu nome? ")
@@ -39,33 +41,59 @@ def cadastro_adm():
 
     return adm
 
+def login(cadastros):
+    email = input("Digite seu email: ")
+    senha = input("Digite sua senha: ")
+
+    for cliente in cadastros["clientes"]:
+        if cliente["email"] == email and cliente["senha"] == senha:
+            print("Login realizado com sucesso (Cliente)!")
+            return
+
+    for adm in cadastros["administradores"]:
+        if adm["email"] == email and adm["senha"] == senha:
+            print("Login realizado com sucesso (Administrador)!")
+            return
+
+    print("Email ou senha incorretos.")
+
+def salvar_dados(cadastros, nome_arquivo="cadastros.json"):
+    with open(nome_arquivo, "w") as arquivo:
+        json.dump(cadastros, arquivo, indent=4)
+
+def carregar_dados(nome_arquivo="cadastros.json"):
+    if os.path.exists(nome_arquivo):
+        with open(nome_arquivo, "r") as arquivo:
+            return json.load(arquivo)
+    else:
+        return {"clientes": [], "administradores": []}
 
 if __name__ == '__main__':
-    cadastros = { 
-        "clientes": ["Clientes:"],
-        "administradores": ["Admnistradores:"]
-    }
+    cadastros = carregar_dados()
 
     while True:
-        escolha = input("Qual cadastro deseja realizar? 1.Cliente 2.Administrador 3.Sair: ")
+        escolha = input("Qual operação deseja realizar? 1.Cliente 2.Administrador 3.Login 4.Sair: ")
 
         if escolha == '1':
             cliente = cadastro_cliente()
             cadastros["clientes"].append(cliente)
+            salvar_dados(cadastros)
             print("Cliente cadastrado com sucesso!")
-            print(cadastros["clientes"])
 
         elif escolha == '2':
             codigo = input("Digite o código de acesso para administradores: ")
             if codigo.lower() == "banana":
                 adm = cadastro_adm()
                 cadastros["administradores"].append(adm)
+                salvar_dados(cadastros)
                 print("Administrador cadastrado com sucesso!")
-                print(cadastros["administradores"])
             else:
                 print("Código de acesso inválido!")
 
         elif escolha == '3':
+            login(cadastros)
+
+        elif escolha == '4':
             break
 
         else:
